f1329679bdeda090c8bf5a1ba0b2436c0ac34fd8
frau-muller <frau-muller@tut.by>
2022-05-01 00:13:34 +0300

    api

--- Api crypt.py
+++ Api crypt.py
@@ -9,4 +9,17 @@ def get_info():
         return response.text
 
 
-def 
+def get_ticker():
+    response = requests.get( url = "https://yobit.net/api/3/info")
+
+    with open ('info.txt',"w") as file:
+        file.write(response.text)
+        return response.text
+
+
+
+def main():
+    print(get_info())
+
+if __name__=="__main__":
+    main()
